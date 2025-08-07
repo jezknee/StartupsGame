23554151e82ab0e45904a934708f34679fe7f213
Jeremy Kneebone <jezknee@gmail.com>
2025-08-07 17:18:01 +0100

    v0.7 antoi monopoly logic complete

--- startups.py
+++ startups.py
@@ -5,7 +5,15 @@ class Company:
         self._name = name
         self._total_shares = total_shares
         self._current_shares = current_shares
-
+    def get_share_count(self, player):
+        share_count = 0
+        for card in player._shares:
+            if card._company == self._name:
+                share_count += 1
+        return share_count
+    def get_majority_holder(self, player_list):
+        for p in player_list:
+            pass
     def __str__(self):
         return f"(Name: {self._name}, Total Shares: {self._total_shares}, Current Shares: {self._current_shares})"
 
@@ -27,16 +35,19 @@ class Player:
         print(f"Type the coins")
         coins_input = int(input())
         if company_in_market(market, company_input):
-            completed = False
-            for c in market:
-                while completed == False:
-                    if c._company == company_input and c._coins_on == coins_input:
-                        c_zero = c
-                        c_zero._coins_on = 0
-                        self._hand.append(c_zero)
-                        self._coins += c._coins_on
-                        market.remove(c)
-                        completed = True
+            if self.check_for_chip(company_input) == False:  
+                completed = False
+                for c in market:
+                    while completed == False:
+                        if c._company == company_input and c._coins_on == coins_input:
+                            c_zero = c
+                            self._coins += c._coins_on
+                            c_zero._coins_on = 0
+                            self._hand.append(c_zero)
+                            market.remove(c)
+                            completed = True
+            elif self.check_for_chip(company_input):
+                print("You've got the anti-monopoly chip for that company, you can't play it")
         else:
             print("There isn't one of these in the market.")
     def add_card_to_shares(self, card):
@@ -45,10 +56,26 @@ class Player:
     def add_card_to_market(self, card, market):
         market.append(card)
         self._hand.remove(card)
-    def add_chip(self, company):
-        self._chips.append(company)
-    def remove_chip(self, company):
-        self._chips.remove(company)
+    def add_chip(self, company, player_list):
+        if check_for_monopoly(player_list, company) == False:
+            self._chips.add(company)
+        else:
+            share_count = company.get_share_count(self)
+            max_shares = find_monopoly_value(player_list, company)
+            if share_count > max_shares:
+                self._chips.add(company)
+    def remove_chip(self, company, player_list):
+        if company in self._chips:
+            share_count = company.get_share_count(self)
+            max_shares = find_monopoly_value(player_list, company)
+            if share_count < max_shares:
+                self._chips.remove(company)
+    def check_for_chip(self, company_name):
+        has_monopoly = False
+        for chip in self._chips:
+            if chip._name == company_name:
+                has_monopoly = True 
+        return has_monopoly
     def __str__(self):
         return f"(Player Number: {self._number}, Current Coins: {self._coins}, Hand: {self._hand}, Shares: {self._shares}, Anti-Monopoly Chips: {self._chips}, Human: {self._human})"
 
@@ -65,7 +92,7 @@ class Card:
         completed = False
         while completed == False:
             for card in market:
-                if c._company == self._company:
+                if card._company == self._company:
                     in_market = True
             completed = True
         return in_market
@@ -105,10 +132,10 @@ def create_players(no_players, no_humans):
     humans_created = 0
     while n <= no_players:
         if humans_created < no_humans:
-            player = Player(n, 10, [], [], [], True)
+            player = Player(n, 10, [], [], set(), True)
             humans_created += 1
         elif humans_created == no_humans:
-            player = Player(n, 10, [], [], [] False)
+            player = Player(n, 10, [], [], set(), False)
         player_list.append(player)
         n += 1
     return player_list
@@ -167,12 +194,17 @@ def get_card_list(list):
 
 def picking_up_card(player, action, market, deck):
     if action == "deck":
-        if player._coins >= len(market):
+        coins_required = len(market)
+        for card in market:
+            if player.check_for_chip(card._company):
+                coins_required -= 1
+        if player._coins >= coins_required:
             player.take_card_from_pile(deck)
-            if len(market) != 0:
+            if coins_required != 0:
                 for card in market:
-                    card._coins_on += 1
-                    player._coins -= len(market)
+                    if player.check_for_chip(card._company) == False:
+                        card._coins_on += 1
+                player._coins -= coins_required
         else:
             print("You don't have enough money to pick up from the market.")
     elif action == "market":
@@ -181,7 +213,7 @@ def picking_up_card(player, action, market, deck):
         else:
             print("There aren't any cards in the market right now.")
 
-def putting_down_card(player, action, market):
+def putting_down_card(player, action, player_list, market, company_list):
     if action == 'front':
         print(f"Choose a card: {get_card_dictionary(player._hand)}")
         card_company = input()
@@ -189,7 +221,15 @@ def putting_down_card(player, action, market):
             if c._company == card_company:
                 chosen_card = c
                 break
+        company_obj = None
+        for comp in company_list:
+            if comp._name == card_company:
+                company_obj = comp
+                break
         player.add_card_to_shares(chosen_card)
+        player.add_chip(company_obj, player_list)
+        for p in player_list:
+            p.remove_chip(company_obj, player_list)
     elif action == 'market':
         print(f"Choose a card: {get_card_dictionary(player._hand)}")
         card_company = input()
@@ -201,11 +241,23 @@ def putting_down_card(player, action, market):
         player.add_card_to_market(chosen_card, market)
 
 
+def check_for_monopoly(player_list, company):
+    monopoly = False
+    for p in player_list:
+        player_shares = company.get_share_count(p)
+        if player_shares > 0:
+            monopoly = True
+    return monopoly
 
-def check_for_monopoly(player, company):
+def find_monopoly_value(player_list, company):
+    max_shares = 0
+    for p in player_list:
+        player_shares = company.get_share_count(p)
+        if player_shares > max_shares:
+            max_shares = player_shares
+    return max_shares
 
-
-
+        
 if __name__ == "__main__":
     company_list = create_companies(default_companies)
     player_list = create_players(4, 1)
@@ -235,9 +287,10 @@ if __name__ == "__main__":
                 print(f"You now have {p._coins} coins")
                 print("Put a card down in fron of you, or put a card into the market. Type 'front' or 'market'.")
                 put_down_action = input()
-                putting_down_card(p, put_down_action, market)
+                putting_down_card(p, put_down_action, player_list, market, company_list)
                 print(f"Your hand is now: {get_card_dictionary(p._hand)}")
                 print(f"Your shares are now: {get_card_dictionary(p._shares)}")
+                print(f"Your anti-monopoly chips are now {p._chips}")
 
                 Finished = True
 
