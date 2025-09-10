a0a74bc5c83673a978ec709336c3b007adfbc367
Jeremy Kneebone <jezknee@gmail.com>
2025-09-10 17:24:02 +0100

    v3.9 reducing printing, committing for GPU

--- runner2.py
+++ runner2.py
@@ -66,7 +66,7 @@ if __name__ == '__main__':
     
         #print("Creating agent...")
         # make input_dims match the observation space without hardcoding
-        agent = Agent(alpha=0.001, gamma=0.99, n_actions=env.action_space.n, epsilon=1.0, batch_size=128, input_dims=env.observation_space.shape[0], epsilon_dec=0.9995, epsilon_end=0.05, mem_size=1000000, fname='C:\\Users\\jezkn\\OneDrive\\Documents\\Startups\\StartupsGame\\startup_model_win.keras')
+        agent = Agent(alpha=0.001, gamma=0.99, n_actions=env.action_space.n, epsilon=1.0, batch_size=128, input_dims=env.observation_space.shape[0], epsilon_dec=0.9995, epsilon_end=0.05, mem_size=1000000, fname='C:\\Users\\jezkn\\OneDrive\\Documents\\Startups\\StartupsGame\\startup_model_bigtrain.keras')
         #print("Agent created successfully")
 
         game_history = []
@@ -74,7 +74,7 @@ if __name__ == '__main__':
 
         scores = []
         eps_history = []
-        num_episodes = 1000
+        num_episodes = 10000
 
         for i in range(num_episodes):
             #print(f"Starting episode {i}")
